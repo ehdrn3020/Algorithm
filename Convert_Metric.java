a4ad632743553fc738fc18d8e04f79de89491a11
DongGu Kang <ehdrnsla3020@naver.com>
2017-09-05 20:51:22 +0900

    Update and rename Convert Metric to Convert_Metric.java

--- Convert_Metric.java
+++ Convert_Metric.java
@@ -1,19 +1,19 @@
 /*
 ###Convert###
 
-# Type	   Metric	            English equivalent
-# Weight	 1.000 kilogram	    2.2046 pounds
-#          0.4536 kilograms	  1.0000 pound
-# Volume	 1.000 liter	      0.2642 gallons
-#          3.7854 liters	    1.000 gallon
+Type	   Metric	            English equivalent
+Weight	   1.000 kilogram	    2.2046 pounds
+           0.4536 kilograms	    1.0000 pound
+Volume	   1.000 liter	            0.2642 gallons
+           3.7854 liters	    1.000 gallon
 
 #Input
-#The first line of input contains a single integer N, (1 ≤ N ≤ 1000) which is the number of datasets that follow.
-#Each dataset consists of a single line of input containing a floating point (double precision) number, a space and the unit specification for the measurement to be converted. The unit specification is one of kg, lb, l, or g referring to kilograms, pounds, liters and gallons respectively.
+The first line of input contains a single integer N, (1 ≤ N ≤ 1000) which is the number of datasets that follow.
+Each dataset consists of a single line of input containing a floating point (double precision) number, a space and the unit specification for the measurement to be converted. The unit specification is one of kg, lb, l, or g referring to kilograms, pounds, liters and gallons respectively.
 
 #Output
-#For each dataset, you should generate one line of output with the following values: The dataset
-#number as a decimal integer (start counting at one), a space, and the appropriately converted value rounded to 4 decimal places, a space and the unit specification for the converted value.
+For each dataset, you should generate one line of output with the following values: The dataset
+number as a decimal integer (start counting at one), a space, and the appropriately converted value rounded to 4 decimal places, a space and the unit specification for the converted value.
 
 EX input
 5
